7193eab3566acb8bcfb1200a38ac7ecb5afe1606
abdulaziz <aboudecharkawi@gmail.com>
2026-02-24 12:52:57 +0200

    cv

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -105,7 +105,7 @@ export function HeroSection() {
               Get In Touch
             </Button>
 
-            <a href="/mycv.pdf" download="Abdelaziz-CV.pdf">
+            <a href="/mycv2.pdf" download="Abdelaziz-CV.pdf">
               <Button
                 variant="outline"
                 size="lg"
